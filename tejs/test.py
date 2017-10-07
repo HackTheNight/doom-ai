fd2eafda595ff9d17b90d62f9f5588e53a0d8530
Matej Navara <matej.navara@gmail.com>
2017-10-07 22:49:55 +0100

    Added matplotlib graph

--- tejs/test.py
+++ tejs/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 from vizdoom import DoomGame
-import random
-import time
+from matplotlib import pyplot as plt
+import random, time, numpy
 
 game = DoomGame()
 game.load_config("./custom-basic.cfg")
@@ -19,6 +19,8 @@ jump = [0, 0, 0, 0, 0, 0, 0, 1]
 actions = [shoot, left, right, forward, backward, turn_left, turn_right, jump]
 
 episodes = 10
+resultplot = []
+
 for i in range(episodes):
     game.new_episode()
     while not game.is_episode_finished():
@@ -29,4 +31,19 @@ for i in range(episodes):
         print("reward:", reward)
         time.sleep(0.02)
     print("Result:", game.get_total_reward())
+    resultplot.append(game.get_total_reward())
     time.sleep(2)
+
+
+"""Plots the results using matplotlib"""
+"""Converts the global result lists into numpy arrays for use in matplotlib"""
+resultnpy = numpy.array(resultplot)
+
+x = numpy.linspace(0, len(resultnpy), len(resultnpy))
+
+with plt.style.context('fivethirtyeight'):
+    """Graph for average fitness over time"""
+    fig = plt.figure()
+    fig.suptitle('Results Over Time', fontsize=14, fontweight='bold')
+    plt.plot(x, resultnpy, color='red')
+    plt.show()
